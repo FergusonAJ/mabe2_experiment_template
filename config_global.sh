d2f69c7587c50a33e5c8d2eda0d6d2fdbd71b2b1
Austin Ferguson <ferguson.austin.j@gmail.com>
2024-03-12 14:10:42 -0400

    Add global config file

---
+++ config_global.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This file contains variables that are shared across ALL experiments
+
+# Grab the full path to the root directory of the repository 
+REPO_ROOT_DIR=
+# All files on the scratch will live in some subdirectory of this
+SCRATCH_ROOT_DIR=
+# Where is the user's global roll_q?
+ROLL_Q_DIR=
