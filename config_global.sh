d8edfb5a9638bc1b94f8988c1619867e27142f41
Austin Ferguson <ferguson.austin.j@gmail.com>
2024-03-12 23:25:42 -0400

    Add options to config_global.sh

--- config_global.sh
+++ config_global.sh
@@ -4,7 +4,12 @@
 
 # Grab the full path to the root directory of the repository 
 REPO_ROOT_DIR=
+
 # All files on the scratch will live in some subdirectory of this
 SCRATCH_ROOT_DIR=
+
 # Where is the user's global roll_q?
-ROLL_Q_DIR=
+ROLL_Q_DIR=${SCRATCH_ROOT_DIR}/roll_q
+
+# Where is the local copy of roll_q? (Likely a submodule in the repo)
+BASE_ROLL_Q_DIR=${REPO_ROOT_DIR}/roll_q
